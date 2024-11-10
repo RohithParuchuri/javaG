f630936721a40a1cb29d4e7f33259eb0f20fbf77
Rohith <mr.rohith.paruchuri@gmail.com>
2024-11-10 10:10:56 +0530

    minor tweak

--- SimpleMouseClickTracker.java
+++ SimpleMouseClickTracker.java
@@ -6,14 +6,14 @@ public class SimpleMouseClickTracker {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        Robot bot = new Robot();
         System.out.print("Enter Delay: ");
         int time = sc.nextInt();
         System.out.print("Number of repetition: ");
         int rep = sc.nextInt();
         int mask = InputEvent.BUTTON1_DOWN_MASK;
-
+        
         try {
+            Robot bot = new Robot();
             for (int i = 0; i < rep; i++) { // Click rep times
                 Thread.sleep(time);
                 bot.mousePress(mask);
