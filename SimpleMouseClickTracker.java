a548546ba1772b9fa654af828e2b25938db88c18
Rohith <mr.rohith.paruchuri@gmail.com>
2024-11-10 10:07:04 +0530

    Added AutoClick ability with CLI

---
+++ SimpleMouseClickTracker.java
@@ -0,0 +1,26 @@
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.util.Scanner;
+
+public class SimpleMouseClickTracker {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        Robot bot = new Robot();
+        System.out.print("Enter Delay: ");
+        int time = sc.nextInt();
+        System.out.print("Number of repetition: ");
+        int rep = sc.nextInt();
+        int mask = InputEvent.BUTTON1_DOWN_MASK;
+
+        try {
+            for (int i = 0; i < rep; i++) { // Click rep times
+                Thread.sleep(time);
+                bot.mousePress(mask);
+                bot.mouseRelease(mask);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Print any exceptions that occur
+        }
+    }
+}
